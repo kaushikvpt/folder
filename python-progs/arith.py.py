7453e59b23f26894e0c3c9e5d805545930a20382
kaushikvpt <kaushikd4527@gmail.com>
2023-08-24 12:20:15 +0530

    added div function to arith.py in python-progs directory

--- python-progs/arith.py.py
+++ python-progs/arith.py.py
@@ -4,6 +4,8 @@ def sub(n1,n2):
    return(n1-n2)
 def mul(n1,n2):
    return(n1*n2)
+def div(n1,n2):
+   return(n1/n2)
 
  
 a=int(input("Enter num1:"))
@@ -11,3 +13,4 @@ b=int(input("Enter num2:"))
 print('sum=',add(a,b))
 print('difference=',sub(a,b))
 print('product=',mul(a,b))
+print('reminder=',div(a,b))
