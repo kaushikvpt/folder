0acd6bb908ba6838e31f6ca6bfdb93f63721c49d
kaushikvpt <kaushikd4527@gmail.com>
2023-08-24 12:11:44 +0530

    added mul function to arith.py in python-progs directory

--- python-progs/arith.py.py
+++ python-progs/arith.py.py
@@ -1,10 +1,13 @@
-def add(n1,n2)
+def add(n1,n2):
    return(n1+n2)
-def sub(n1,n2)
+def sub(n1,n2):
    return(n1-n2)
+def mul(n1,n2):
+   return(n1*n2)
 
  
 a=int(input("Enter num1:"))
 b=int(input("Enter num2:"))
 print('sum=',add(a,b))
 print('difference=',sub(a,b))
+print('product=',mul(a,b))
